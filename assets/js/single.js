e94a02a399c7d822e083b7d50bda719eb092881b
TaimurHasan <taimurhasan11@gmail.com>
2022-03-22 22:01:44 -0400

    finalized issue page js

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,4 +1,6 @@
 var issueContainerEl = document.querySelector("#issues-container");
+var limitWarningEl = document.querySelector("#limit-warning");
+
 
 var getRepoIssues = function(repo) {
     var apiUrl = "https://api.github.com/repos/" + repo + "/issues?direction=asc";
@@ -9,6 +11,10 @@ var getRepoIssues = function(repo) {
                 response.json()
                 .then(function(data) {
                     displayIssues(data);
+
+                    if(response.headers.get("Link")) {
+                        displayWarning(repo);
+                    }
                 });
             } else {
                 alert("There was a problem with your request!");
@@ -46,5 +52,16 @@ var displayIssues = function(issues) {
     }
 
     
+}
+
+var displayWarning = function(repo) {
+    limitWarningEl.textContent = "To see more than 30 issues, visit ";
+
+    var linkEl = document.createElement("a");
+    linkEl.textContent = "See More Issues on GitHub.com";
+    linkEl.setAttribute("href", "https://github.com/" + repo + "/issues");
+    linkEl.setAttribute("target", "_blank");
+
+    limitWarningEl.appendChild(linkEl);
 }
 getRepoIssues("facebook/react");
