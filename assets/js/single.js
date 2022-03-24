ea9f3ea6b3709de97a85863d7d0750bced48f76a
TaimurHasan <taimurhasan11@gmail.com>
2022-03-23 20:35:03 -0400

    feeding repo data into separate html file

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,7 +1,18 @@
 var issueContainerEl = document.querySelector("#issues-container");
 var limitWarningEl = document.querySelector("#limit-warning");
+var repoNameEl = document.querySelector("#repo-name");
 
-
+var getRepoName = function () {
+    var queryString = document.location.search;
+    var repoName = queryString.split("=")[1];
+    if(repoName) {
+        getRepoIssues(repoName);
+        repoNameEl.textContent = repoName;
+    } else {
+        document.location.replace("./index.html");
+    }
+    
+}
 var getRepoIssues = function(repo) {
     var apiUrl = "https://api.github.com/repos/" + repo + "/issues?direction=asc";
 
@@ -17,7 +28,7 @@ var getRepoIssues = function(repo) {
                     }
                 });
             } else {
-                alert("There was a problem with your request!");
+                document.location.replace("./index.html");
             }
         })
 };
@@ -64,4 +75,4 @@ var displayWarning = function(repo) {
 
     limitWarningEl.appendChild(linkEl);
 }
-getRepoIssues("facebook/react");
+getRepoName();
