79d2e3d942f9087a20d1f710b4031f5aa9a53948
TaimurHasan <taimurhasan11@gmail.com>
2022-03-23 21:09:52 -0400

    add language buttons for alternate search

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,5 +1,6 @@
 var repoContainerEl = document.querySelector("#repos-container");
 var repoSearchTerm = document.querySelector("#repo-search-term");
+var languageButtonsEl = document.querySelector("#language-buttons");
 
 
 var getUserRepos = function(user) {
@@ -74,7 +75,30 @@ var formSubmitHandler = function(event) {
     }
 }
 
+var getFeaturedRepos = function (language) {
+    var apiUrl = "https://api.github.com/search/repositories?q=" + language + "+is:featured&sort=help-wanted-issues";
+    fetch(apiUrl).then(function(response) {
+        if(response.ok) {
+            response.json().then(function(data) {
+                    displayRepos(data.items, language);
+            })
+        } else {
+            alert('Error: GitHub User Not Found');
+        }
+    });
+}
+
+var buttonClickHandler = function(event){
+    var language = event.target.getAttribute("data-language");
+    if(language) {
+        getFeaturedRepos(language);
+        repoContainerEl.textContent = "";
+    }
+}
+
 userFormEl.addEventListener("submit", formSubmitHandler);
+languageButtonsEl.addEventListener("click", buttonClickHandler);
+
 
 
 
